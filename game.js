90065263f134108314e52e18b5c9a6a2ad3e75d5
Derek <kleidere942@gmail.com>
2026-07-05 19:13:23 -0600

    Cleared turn counter when game draws

--- game.js
+++ game.js
@@ -1,56 +1,62 @@
 const cells = document.querySelectorAll(".cell")
 const turnCounter = document.querySelector(".turn-counter")
-document.querySelector(".reset").addEventListener("click",reset);
-for (let i = 0; i<9; i++){
-    cells[i].addEventListener("click", () => {test(i)})
+document.querySelector(".reset").addEventListener("click", reset);
+for (let i = 0; i < 9; i++) {
+    cells[i].addEventListener("click", () => { test(i) })
 }
 
-var i=0;
+var i = 0;
 
-function test(number){
+function test(number) {
     if (cells[number].textContent != "")
         return
     i++
-    cells[number].textContent = i % 2 ===0 ? "X" : "O" 
-    turnCounter.textContent = `Player ${i % 2 ===0 ? "O" : "X"} Turn`
-    if  (whoHasWon() != null){
+    cells[number].textContent = i % 2 === 0 ? "X" : "O"
+    turnCounter.textContent = `Player ${i % 2 === 0 ? "O" : "X"} Turn`
+    if (whoHasWon() != null) {
         alert(`${whoHasWon()} has won!`)
-        reset();
+        setTimeout(reset, 500);
+    }else if(i == 9){
+
+        turnCounter.textContent = "   ";
+        alert("Draw")
+        setTimeout(reset,500);
     }
 }
 
-function reset(){
-    i=0;
-    turnCounter.textContent = `Player ${i % 2 ===0 ? "O" : "X"} Turn`
-for (let i = 0; i<9; i++){
-    cells[i].textContent = ""
+function reset() {
+    i = 0;
+    turnCounter.textContent = `Player ${i % 2 === 0 ? "O" : "X"} Turn`
+    for (let i = 0; i < 9; i++) {
+        cells[i].textContent = ""
 
-}
+    }
+
 }
 
 //0 for no one
 //1 for X
 //2 for O
-function whoHasWon(){
-    for (let i = 1; i<3;i++){
+function whoHasWon() {
+    for (let i = 1; i < 3; i++) {
         let char = i == 1 ? "X" : "O"
-        if (cells[0].textContent==char && cells[1].textContent==char && cells[2].textContent==char)
+        if (cells[0].textContent == char && cells[1].textContent == char && cells[2].textContent == char)
             return char
-        if (cells[3].textContent==char && cells[4].textContent==char && cells[5].textContent==char)
+        if (cells[3].textContent == char && cells[4].textContent == char && cells[5].textContent == char)
             return char
-        if (cells[6].textContent==char && cells[7].textContent==char && cells[8].textContent==char)
-            return char
-
-        if (cells[0].textContent==char && cells[3].textContent==char && cells[6].textContent==char)
-            return char
-        if (cells[1].textContent==char && cells[4].textContent==char && cells[7].textContent==char)
-            return char
-        if (cells[2].textContent==char && cells[5].textContent==char && cells[8].textContent==char)
+        if (cells[6].textContent == char && cells[7].textContent == char && cells[8].textContent == char)
             return char
 
-         if (cells[0].textContent==char && cells[4].textContent==char && cells[8].textContent==char)
+        if (cells[0].textContent == char && cells[3].textContent == char && cells[6].textContent == char)
             return char
-        if (cells[2].textContent==char && cells[4].textContent==char && cells[6].textContent==char)
+        if (cells[1].textContent == char && cells[4].textContent == char && cells[7].textContent == char)
+            return char
+        if (cells[2].textContent == char && cells[5].textContent == char && cells[8].textContent == char)
+            return char
+
+        if (cells[0].textContent == char && cells[4].textContent == char && cells[8].textContent == char)
+            return char
+        if (cells[2].textContent == char && cells[4].textContent == char && cells[6].textContent == char)
             return char
     }
     return null;
